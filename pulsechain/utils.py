affd16d2b5379f058966f2dc6c3d5669b7210e87
CryptoRalph <66646290+crypto-ralph@users.noreply.github.com>
2023-05-28 19:32:04 +0200

    Initial Commit 2

---
+++ pulsechain/utils.py
@@ -0,0 +1,12 @@
+def add_decimal_sign(string: str, decimal_places: int = 18) -> str:
+    """
+    Insert a dot into a string at a specific position from the right.
+
+    :param string: The string to insert the dot into.
+    :type string: str
+    :param decimal_places: The number of decimal places to separate from the right.
+    :type decimal_places: int
+    :return: The string with the dot inserted.
+    :rtype: str
+    """
+    return string[:-decimal_places] + "." + string[-decimal_places:]
