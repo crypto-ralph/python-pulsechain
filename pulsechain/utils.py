5adbdd590c9c0e3a0361200429fc2f0da260acaf
CryptoRalph <doxter@protonmail.com>
2023-06-07 23:01:30 +0200

    Fix pylint issues in utils.py

--- pulsechain/utils.py
+++ pulsechain/utils.py
@@ -3,6 +3,7 @@ utils.py
 
 This module provides helper functions.
 """
+import requests
 
 
 def add_decimal_sign(string: str, decimal_places: int = 18) -> str:
@@ -20,7 +21,11 @@ def add_decimal_sign(string: str, decimal_places: int = 18) -> str:
 
 
 def check_result(response):
+    """
+
+    :param response: Response from the API to be checked.
+    :return: Unwrapped result if the call was successful.
+    """
     if response["message"] == "OK":
         return response["result"]
-    else:
-        raise Exception(response["message"])
+    raise requests.exceptions.RequestException(response["message"])
